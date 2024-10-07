37e41256cc8f6044cbc9d7d6f49f3efe690080fb
Tardigrade180 <abdallahhussein595@gmail.com>
2024-10-07 11:09:51 +0000

    Added BookList template with api

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <div>
       <Navbar /> 
       <HeroSearch />
+      <BookList/>
     </div>
   )
 }
---
+++ src/components/BookList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const BookList = ({ books }) => {
+  return (
+    <div className="Bookgrid grid-flow-rowgap-4">
+      {books.map((book) => (
+        <div key={book.id} className="book-item">
+          <div className="book-image">
+            <img src={book.volumeInfo.imageLinks?.thumbnail} alt={book.volumeInfo.title} />
+          </div>
+          <div className="book-info">
+            <h3>{book.volumeInfo.title}</h3>
+            <p>{book.volumeInfo.authors?.join(', ')}</p>  {/* Joining multiple authors */}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BookList;
