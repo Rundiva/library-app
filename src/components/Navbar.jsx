cfad7322acfee4ecd373a4c64b91ee2c3a9057be
Faiza Abdul-Wahab <faizaabdulwahab@ymail.com>
2024-10-10 18:35:47 +0000

    Booklist worked on

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ import { FaUserLock } from "react-icons/fa";
 
 const Navbar = () => {
     return (
-        <div>
+        <div >
 
-            <nav className='flex place-items-center justify-center text-center text-lg fixed text-zinc-50 space-x-10 p-7 w-[100vw] font-bold bg-[#f8a8288f]'>
+            <nav className=' flex place-items-center justify-center text-center text-lg fixed text-zinc-50 space-x-10 p-7 w-[100vw] font-bold bg-[#f8a8288f]'>
                 <div id="NavLogo" className="flex flex-row w-[5rem] h-[2rem]">
                     <img src={NavLogo} alt="img" />
 
--- src/pages/bookList/BookList.jsx
+++ src/pages/bookList/BookList.jsx
@@ -3,33 +3,46 @@ import Hero from "../bookList/Hero";
 // import Hero from "./components/Hero";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import K from "../../constants";
+import  { BASE_URL } from "../../constants";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const BookList = () => {
 
+  const [books, setbooks] = useState([]);
+
+  const getBooks = async () => {
+    const response = await axios.get(`${BASE_URL}/books`)
+    setbooks(response.data);
+  }
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+
 
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <div classname="grid grid-cols-3 w-3/2 mx-auto my-24 gap-12">
-      {K.BOOKS.map((book,index) => {
-        console.log(`${index}: ${book.text}`);
-        return <div key={index} className="flex flex-col gap-y-2">
-          {/* <span >{bookCover}</span> */}
-          <h3>{book.text}</h3>
-          <p>{ book.description}</p>
-          </div>
-      })
-      
-    }
-        
-      </div>
-
-      <Footer />
-
+    
+    <div className="grid grid-cols-3 w-4/5 mx-auto my-24 gap-12">
+       {books.map((book,index) => {
+    console.log( book);
+    return <div key={index} className="flex flex-col gap-y-2">
+       <img src={book.cover} alt="pic" />
+        <h3 className="text-2xl font-semibold">{book.title}</h3>
+        <p>{book.summary}</p>
     </div>
+
+})}
+        </div>
   );
+        
+
+     
+
+     
+
+   
+  
 };
 
 export default BookList;
