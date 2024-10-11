7bd7ed8ec7210207bf871154033f72fcfdbd63b4
Tardigrade180 <abdallahhussein595@gmail.com>
2024-10-11 09:24:33 +0000

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <div >
 
-            <nav className=' flex place-items-center justify-center text-center text-lg fixed text-zinc-50 space-x-10 p-7 w-[100vw] font-bold bg-[#f8a8288f]'>
+            <nav className=' flex place-items-center justify-center text-center text-lg fixed text-zinc-50 space-x-10 p-7 w-[100vw] font-bold bg-transparent backdrop-blur-sm'>
                 <div id="NavLogo" className="flex flex-row w-[5rem] h-[2rem]">
                     <img src={NavLogo} alt="img" />
 
